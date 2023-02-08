fa43ac2e9fb60d43841ff111b699ab844e27a4e6
DheniMoura <dhenifer@hotmail.com>
2023-02-07 23:08:34 -0300

    Add Curso.java, Mentoria.java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso c1 = new Curso("Java", "Curso online", 15);
+        Mentoria m1 = new Mentoria("JS", "Mentoria em JavaScript", (LocalDate.now()));
+
+        System.out.println(c1.toString());
+        System.out.println(m1.toString());
+
     }
 }
